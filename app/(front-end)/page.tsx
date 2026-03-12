a9be30833a5942e7edf0a5f90e18d069c184bbea
viniciusofp <viniciusofp@gmail.com>
2026-03-12 16:06:56 -0300

    add page title

--- app/(front-end)/page.tsx
+++ app/(front-end)/page.tsx
@@ -1,5 +1,9 @@
 import MainWrapper from '@/components/MainWrapper';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Observatório de Violência Política de Gênero'
+};
 export default function Page() {
   return (
     <>
