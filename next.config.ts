58f12f8ef055e679905b9227f8bd49ec04587eaa
viniciusofp <viniciusofp@gmail.com>
2026-03-12 14:46:56 -0300

    update versions insite yml flow

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  basePath: '/observatorio-vpg'
+};
+
+export default nextConfig;
